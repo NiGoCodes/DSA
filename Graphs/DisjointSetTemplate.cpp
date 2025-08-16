1bebd5eb1095d0c5cc2923916460859adf1bb65b
Nikhilesh Goswami <169056827+NiGoCodes@users.noreply.github.com>
2025-08-16 15:51:02 +0530

    Update DisjointSetTemplate.cpp

--- Graphs/DisjointSetTemplate.cpp
+++ Graphs/DisjointSetTemplate.cpp
@@ -1,40 +1,34 @@
 class DisjointSet{
-	vector<int> rank , parent ,size;
-	int n;
-	public:
-	DisjointSet(int x){
-		int n = x;
-		rank.resize(n+1, 1);
-		parent.resize(n+1, 1);
-		rank.resize(n+1, 1);
-		for(int i = 0 ; i <= n ; i++) parent[i] = i;
-	}	
-	
-	int find(int u){
-		if(parent[u] == u) return u;
-		return parent[u] = find(parent[u]);
-	}
-	
-	void merge(int u , int v){
-		int ult_par_u = find(u);
-		int ult_par_v = find(v);
+    vector<int> rank , parent;
+    int n;
+    public:
+    DisjointSet(int x){
+        n = x;
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        for(int i = 0 ; i <= n ; i++) parent[i] = i;
+    }   
+    
+    int find(int u){
+        if(parent[u] == u) return u;
+        return parent[u] = find(parent[u]);
+    }
+    
+    void merge(int u , int v){
+        int ult_par_u = find(u);
+        int ult_par_v = find(v);
 
-		if(ult_par_u == ult_par_v) return ;
+        if(ult_par_u == ult_par_v) return ;
 
-		if(rank[ult_par_u] > rank[ult_par_v]){
-			parent[ult_par_v] = parent[ult_par_u];
-		}
-		else if(rank[ult_par_v] < rank[ult_par_u]){
-			parent[ult_par_u] = parent[ult_par_v];
-		}
-		else{
-			parent[ult_par_u] = parent[ult_par_v];
-			rank[ult_par_v]++;
-		}
-	}
-
-	void reset(){
-		for(int i = 0 ; i <= n ; i++) parent[i] = i;
-		for(int i = 0 ; i <= n ; i++) rank[i] = 0;	
-	}
-};
+        if(rank[ult_par_u] > rank[ult_par_v]){
+            parent[ult_par_v] = ult_par_u;
+        }
+        else if(rank[ult_par_v] > rank[ult_par_u]){
+            parent[ult_par_u] = ult_par_v;
+        }
+        else{
+            parent[ult_par_u] = ult_par_v;
+            rank[ult_par_v]++;
+        }
+    }
+  };
